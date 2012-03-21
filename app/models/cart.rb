e5c97f0da3ef7368b34364a6929665b868e00d0c
pengquankun <283857115@qq.com>
2012-03-21 22:02:45 +0800

    created an Ajax-Based Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,8 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
+  def total_items
+    line_items.sum(:quantity)
+  end
   def total_price
     line_items.to_a.sum {|item| item.total_price }
   end
